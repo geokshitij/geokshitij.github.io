9c933631fbcce44e5886458c1bfb5cba03e5673c
Kshitij Dahal <geokshitij@gmail.com>
2026-01-28 15:19:00 -0700

    Embed CV Google Doc

--- _pages/cv.md
+++ _pages/cv.md
@@ -5,4 +5,9 @@ permalink: /cv/
 author_profile: true
 ---
 {% include base_path %}
-<a href="https://docs.google.com/document/d/1Mndv6ZTSUXTpm0GEMDLeL1c7mdpGmSMvqs4Vk6foDeA/edit?usp=sharing" class="btn btn--primary" target="_blank">View Full CV (Google Doc)</a>
+
+<iframe src="https://docs.google.com/document/d/1Mndv6ZTSUXTpm0GEMDLeL1c7mdpGmSMvqs4Vk6foDeA/preview" width="100%" height="1000px" frameborder="0"></iframe>
+
+<p style="margin-top: 20px;">
+<a href="https://docs.google.com/document/d/1Mndv6ZTSUXTpm0GEMDLeL1c7mdpGmSMvqs4Vk6foDeA/edit?usp=sharing" class="btn btn--primary" target="_blank">Open in Google Docs</a>
+</p>
--- generate_content.py
+++ generate_content.py
@@ -697,7 +697,12 @@ permalink: /cv/
 author_profile: true
 ---
 {% include base_path %}
-<a href="https://docs.google.com/document/d/1Mndv6ZTSUXTpm0GEMDLeL1c7mdpGmSMvqs4Vk6foDeA/edit?usp=sharing" class="btn btn--primary" target="_blank">View Full CV (Google Doc)</a>
+
+<iframe src="https://docs.google.com/document/d/1Mndv6ZTSUXTpm0GEMDLeL1c7mdpGmSMvqs4Vk6foDeA/preview" width="100%" height="1000px" frameborder="0"></iframe>
+
+<p style="margin-top: 20px;">
+<a href="https://docs.google.com/document/d/1Mndv6ZTSUXTpm0GEMDLeL1c7mdpGmSMvqs4Vk6foDeA/edit?usp=sharing" class="btn btn--primary" target="_blank">Open in Google Docs</a>
+</p>
 """
     generate_page("_pages/cv.md", cv_content)
 
