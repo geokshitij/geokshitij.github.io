45aebc3bf0c06ea4e438a33e87cebd05fa0731c9
Kshitij Dahal <geokshitij@gmail.com>
2026-01-28 15:21:07 -0700

    Move button to top

--- _pages/cv.md
+++ _pages/cv.md
@@ -6,8 +6,8 @@ author_profile: true
 ---
 {% include base_path %}
 
-<iframe src="https://docs.google.com/document/d/1Mndv6ZTSUXTpm0GEMDLeL1c7mdpGmSMvqs4Vk6foDeA/preview" width="100%" height="1000px" frameborder="0"></iframe>
-
-<p style="margin-top: 20px;">
+<p style="margin-bottom: 20px;">
 <a href="https://docs.google.com/document/d/1Mndv6ZTSUXTpm0GEMDLeL1c7mdpGmSMvqs4Vk6foDeA/edit?usp=sharing" class="btn btn--primary" target="_blank">Open in Google Docs</a>
 </p>
+
+<iframe src="https://docs.google.com/document/d/1Mndv6ZTSUXTpm0GEMDLeL1c7mdpGmSMvqs4Vk6foDeA/preview" width="100%" height="1000px" frameborder="0"></iframe>
--- generate_content.py
+++ generate_content.py
@@ -698,11 +698,11 @@ author_profile: true
 ---
 {% include base_path %}
 
-<iframe src="https://docs.google.com/document/d/1Mndv6ZTSUXTpm0GEMDLeL1c7mdpGmSMvqs4Vk6foDeA/preview" width="100%" height="1000px" frameborder="0"></iframe>
-
-<p style="margin-top: 20px;">
+<p style="margin-bottom: 20px;">
 <a href="https://docs.google.com/document/d/1Mndv6ZTSUXTpm0GEMDLeL1c7mdpGmSMvqs4Vk6foDeA/edit?usp=sharing" class="btn btn--primary" target="_blank">Open in Google Docs</a>
 </p>
+
+<iframe src="https://docs.google.com/document/d/1Mndv6ZTSUXTpm0GEMDLeL1c7mdpGmSMvqs4Vk6foDeA/preview" width="100%" height="1000px" frameborder="0"></iframe>
 """
     generate_page("_pages/cv.md", cv_content)
 
